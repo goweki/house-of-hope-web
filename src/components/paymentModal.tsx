81d22b6ad884a8a1ea0e0cdaa648cd3594d572b8
G. Oweki <contact@goweki.com>
2025-08-06 18:19:59 +0300

    meta data

--- src/components/paymentModal.tsx
+++ src/components/paymentModal.tsx
@@ -49,16 +49,10 @@ export function PaymentModal({
 
           <div className="p-4 md:p-5 space-y-4">
             <p className="text-base leading-relaxed text-gray-500">
-              With less than a month to go before the European Union enacts new
-              consumer privacy laws for its citizens, companies around the world
-              are updating their terms of service agreements to comply.
+              Send any contributions and donations to:
             </p>
             <p className="text-base leading-relaxed text-gray-500">
-              The European Union’s General Data Protection Regulation (G.D.P.R.)
-              goes into effect on May 25 and is meant to ensure a common set of
-              data rights in the European Union. It requires organizations to
-              notify users as soon as possible of high-risk data breaches that
-              could personally affect them.
+              M-PESA Paybill Account No. CCVV Name: THE NAME
             </p>
           </div>
 
@@ -68,15 +62,15 @@ export function PaymentModal({
               onClick={() => setVisible(false)}
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
             >
-              I accept
+              OK
             </button>
-            <button
+            {/* <button
               type="button"
               onClick={() => setVisible(false)}
               className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
             >
               Decline
-            </button>
+            </button> */}
           </div>
         </div>
       </div>
